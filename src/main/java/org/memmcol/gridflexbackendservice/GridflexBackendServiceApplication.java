7b5c17dcd685f8425c8002baa70b59ea2fe37d96
H.Soliu <abeeb.ilias@gmail.com>
2025-04-26 06:29:05 -0700

    Initial commit

---
+++ src/main/java/org/memmcol/gridflexbackendservice/GridflexBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package org.memmcol.gridflexbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridflexBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridflexBackendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/memmcol/gridflexbackendservice/GridflexBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.memmcol.gridflexbackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridflexBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
