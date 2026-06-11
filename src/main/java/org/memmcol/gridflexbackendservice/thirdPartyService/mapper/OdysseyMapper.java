d23b43d55ad8d0346c2dce1ee5028d45f3c44639
H.Soliu <abeeb.ilias@gmail.com>
2026-06-11 11:44:11 +0100

    odey api clean up v1

--- src/main/java/org/memmcol/gridflexbackendservice/thirdPartyService/mapper/OdysseyMapper.java
+++ src/main/java/org/memmcol/gridflexbackendservice/thirdPartyService/mapper/OdysseyMapper.java
@@ -124,8 +124,8 @@ public interface OdysseyMapper {
             md.latitude,
             md.longitude,
             COALESCE(adj.status, 'FULL_PAYMENT') AS transactionType,
-            COALESCE(t.id, '') AS transaction_id,
-            COALESCE(t.initial_amount, '') AS amount,
+            COALESCE(t.id, NULL) AS transaction_id,
+            COALESCE(t.initial_amount, 0) AS amount,
             COALESCE(t.unit, '') AS transactionKwh,
             COALESCE(t.created_at, '') AS timestamp,
             'NGN' AS currency
