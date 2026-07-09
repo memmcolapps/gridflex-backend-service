a405a5261e3764f7b4a89bdbf0301d8d489f31dd
H.Soliu <abeeb.ilias@gmail.com>
2026-07-09 15:30:32 +0100

    code clean up

--- src/main/java/org/memmcol/gridflexbackendservice/mapper/HesMapper.java
+++ src/main/java/org/memmcol/gridflexbackendservice/mapper/HesMapper.java
@@ -2216,14 +2216,14 @@ public interface HesMapper {
     })
     List<Schedule> getScheduleData(int page, int size);
 
-    @Select("SELECT obis_codes, name, job_name, job_group, cron_expression " +
+    @Select("SELECT obis_codes, name, job_name, job_group " +
             "FROM profile_list WHERE job_group IN ('profile', 'billing') ")
     @Results({
             @Result(column = "obis_codes", property = "obisCode"),
             @Result(column = "name", property = "name"),
             @Result(column = "job_name", property = "jobName"),
             @Result(column = "job_group", property = "jobGroup"),
-            @Result(column = "cron_expression", property = "cronExpression")
+//            @Result(column = "cron_expression", property = "cronExpression")
     })
     List<Schedule> getProfileEvents();
 
